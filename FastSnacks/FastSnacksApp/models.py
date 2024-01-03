1a7954e607577f0e32b596ce4674991e0124bcbc
Albert Tang <tangalbert919@yahoo.com>
2024-01-02 20:29:25 -0600

    Create new models

--- FastSnacks/FastSnacksApp/models.py
+++ FastSnacks/FastSnacksApp/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+# Extend user model
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    reward_points = models.IntegerField(default=0)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class PaymentMethod(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    card_no = models.CharField(max_length=16, null=False)
+    security_code = models.CharField(max_length=4, null=False)
+    exp_month = models.IntegerField()
+    exp_year = models.IntegerField()
+    account_bal = models.DecimalField(max_digits=7, decimal_places=2)
+
+class SupportTicket(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    info = models.TextField(max_length=10000)
+    date = models.DateField()
+    resolved = models.BooleanField(default=False)
+
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    # TODO: Merge all of these
+    calories = models.IntegerField()
+    carbs = models.IntegerField()
+    fat = models.IntegerField()
+    protein = models.IntegerField()
+    sugar = models.IntegerField()
+
+class VendingMachine(models.Model):
+    location = models.CharField(max_length=255)
+
+class Stock(models.Model):
+    machine = models.OneToOneField(VendingMachine)
+    item = models.OneToOneField(Item)
+    quantity = models.IntegerField()
+
+class Order(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    machine = models.OneToOneField(VendingMachine)
+    item = models.OneToOneField(Item)
+    quantity = models.IntegerField()
+    date = models.DateField()
+
+class Favorites(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    item = models.OneToOneField(Item)
