b2c26c2cc26c0ba466c5a38fd88bc84e92808141
Albert Tang <tangalbert919@yahoo.com>
2024-01-04 17:50:19 -0600

    Add templates and static files

--- FastSnacks/FastSnacksApp/views.py
+++ FastSnacks/FastSnacksApp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Currently a work in progress')
+
+def index(request):
+    return render(request, 'index.html')
---
+++ FastSnacks/static/js/base.js
@@ -0,0 +1,8 @@
+// Bootstrap tooltip initialization
+const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+  
