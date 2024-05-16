25e8eb5d9ad5dd77862aeb0b0733593e9da914c6
Albert Tang <tangalbert919@yahoo.com>
2024-05-16 13:37:09 -0500

    Create OrderAdmin model
    This will allow listing prices in admin panel.

--- FastSnacks/FastSnacksApp/admin.py
+++ FastSnacks/FastSnacksApp/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'machine', 'quantity', 'date', 'price']
+    readonly_fields = ['price']
+
+    def price(self, obj):
+        return obj.price()
+    price.short_description = 'Price'
+
 admin.site.register(Item)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(VendingMachine)
 admin.site.register(Stock)
 admin.site.register(Cart)
