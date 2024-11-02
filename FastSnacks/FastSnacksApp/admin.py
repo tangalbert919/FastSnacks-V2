61d9332c6d5e44598c9ef28576cfa56111a89f71
Albert Tang <tangalbert919@yahoo.com>
2024-11-01 23:39:31 -0500

    Document admin module

--- FastSnacks/FastSnacksApp/admin.py
+++ FastSnacks/FastSnacksApp/admin.py
@@ -1,15 +1,25 @@
-from django.contrib import admin
-from .models import *
+"""
+Admin module for FastSnacks.
+Responsible for determining what will appear in the admin panel and how
+each of them will appear.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Cart, Customer, Favorites, Item, Order, PaymentMethod, \
+    Reward, Stock, VendingMachine, SupportTicket
+
+# Admin models should be created here.
 class OrderAdmin(admin.ModelAdmin):
+    '''Allows for more advanced view of orders in the admin panel.'''
     list_display = ['user', 'machine', 'quantity', 'date', 'price']
     readonly_fields = ['price']
 
     def price(self, obj):
+        '''Fetch the total order price.'''
         return obj.price()
     price.short_description = 'Price'
 
+# All models registered here will be visible in the admin panel.
 admin.site.register(Item)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(VendingMachine)
